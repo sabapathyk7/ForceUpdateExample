f41755b8769fa52d75edcd2674fba4cf944c6bd3
Kanagasabapathy <trksabapathy7@gmail.com>
2024-01-15 18:33:04 +0530

    Initial Commit

---
+++ ForceUpdateExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForceUpdateExample
+//
+//  Created by kanagasabapathy on 15/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
