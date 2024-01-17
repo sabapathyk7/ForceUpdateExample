de6a496854b51e52aca776b1f3c18767bf069c38
Kanagasabapathy <trksabapathy7@gmail.com>
2024-01-17 14:00:56 +0530

    Endpoint for Networking

---
+++ ForceUpdateExample/Extension/AppUpdateEndPoint.swift
@@ -0,0 +1,56 @@
+//
+//  AppUpdateEndPoint.swift
+//  ForceUpdateExample
+//
+//  Created by kanagasabapathy on 16/01/24.
+//
+
+import Foundation
+import NetworkKit
+
+enum AppUpdateEndPoint {
+    case appUpdateMetaData(queryParams: [String: String])
+}
+
+//  https://itunes.apple.com/br/lookup
+//  https://itunes.apple.com/br/lookup?bundleId=361309726
+extension AppUpdateEndPoint: EndPoint {
+    var host: String {
+        "itunes.apple.com"
+    }
+
+    var scheme: String {
+        "https"
+    }
+
+    var path: String {
+        "/br/lookup"
+    }
+
+    var method: NetworkKit.RequestMethod {
+        return .get
+    }
+
+    var header: [String: String]? {
+        return ["Content-Type": "application/json"]
+    }
+
+    var body: [String: String]? {
+        return nil
+    }
+
+    var queryParams: [String: String]? {
+        switch self {
+        case .appUpdateMetaData(queryParams: let queryParams):
+            return queryParams
+        }
+    }
+
+    var pathParams: [String: String]? {
+        return nil
+    }
+}
+
+enum Constants {
+  static let bundleID = ["bundleId": "com.apple.Pages"]
+}
