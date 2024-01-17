397aad701cc63de2a5e45e2d56ab7b67726f63f7
Kanagasabapathy <trksabapathy7@gmail.com>
2024-01-17 14:00:16 +0530

    SwiftUI View

--- ForceUpdateExample/SwiftUI/ForceUpdateSwiftUIView.swift
+++ ForceUpdateExample/SwiftUI/ForceUpdateSwiftUIView.swift
@@ -8,8 +8,59 @@
 import SwiftUI
 
 struct ForceUpdateSwiftUIView: View {
+    @StateObject private var viewModel = ForceUpdateViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            switch viewModel.appStatus {
+            case .upToDate:
+                EmptyView()
+            case .updateAvailable(let version, let storeURL, let releaseNotes):
+                AlertView(version: version, storeURL: storeURL, releaseNotes: releaseNotes)
+            }
+        }
+        .padding()
+    }
+}
+
+struct AlertView: View {
+    @State private var showingAlert = false
+    @State private var forceUpdate = false
+    var version: String
+    var storeURL: URL
+    var releaseNotes: String
+    var body: some View {
+        VStack {
+            // Button to trigger the alert with "Update" and "Cancel" buttons
+            Button("Show Update Alert") {
+                forceUpdate = false
+                showingAlert.toggle()
+            }
+
+            // Button to trigger the force update alert with only "Update" button
+            Button("Show Force Update Alert") {
+                forceUpdate = true
+                showingAlert.toggle()
+            }
+        }
+        .alert(isPresented: $showingAlert) {
+            // Use a ternary operator to conditionally create the Alert
+            return forceUpdate
+            ? Alert(title: Text("Force Update"),
+                    message: Text("A new version is available. Update now?"),
+                    dismissButton: .default(Text("Update"), action: {
+                handleUpdate()
+            }))
+            : Alert(title: Text("Update \(version)"),
+                    message: Text(releaseNotes),
+                    primaryButton: .default(Text("Update"), action: {
+                handleUpdate()
+            }),
+                    secondaryButton: .cancel())
+        }
+    }
+
+    func handleUpdate() {
+        UIApplication.shared.open(storeURL)
     }
 }
 
